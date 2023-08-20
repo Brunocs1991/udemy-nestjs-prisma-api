613be09b23047a854b77b1ee594627f105ec02e6
Bruno <brunocosta.1991@hotmail.com>
2023-08-20 16:48:22 -0300

    implements initial structure to crud users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserDto {
+  admin: boolean;
+  email: string;
+  name: string;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,9 @@
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  admin: boolean;
+  createdAt: Date;
+  email: string;
+  id: number;
+  name: string;
+}
