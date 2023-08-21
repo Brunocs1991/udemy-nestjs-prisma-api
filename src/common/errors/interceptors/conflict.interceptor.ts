890415b10eefacea56d530f1ab9a987d03e67973
Bruno <brunocosta.1991@hotmail.com>
2023-08-21 14:18:44 -0300

    Conflict interceptor

---
+++ src/common/errors/interceptors/conflict.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  CallHandler,
+  ConflictException,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+import { ConflictError } from '../types/ConflictError';
+
+@Injectable()
+export class ConflictInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (error instanceof ConflictError) {
+          throw new ConflictException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
