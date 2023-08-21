d4cb8e28dd942fdfd14fdf2f28fed21dd1221090
Bruno <brunocosta.1991@hotmail.com>
2023-08-21 18:18:40 -0300

    install and configuration swagger

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,17 @@ import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthoriz
 import { NotfoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The simple blog API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,6 +28,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotfoundInterceptor());
+
   await app.listen(process.env.PORT || 3000);
 }
 
