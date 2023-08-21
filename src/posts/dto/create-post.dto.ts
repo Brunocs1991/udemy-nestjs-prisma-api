966757d804adb86c37d55bec6a051467b40716f4
Bruno <brunocosta.1991@hotmail.com>
2023-08-21 16:01:21 -0300

    validation data into create and patch

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+  @IsString()
+  @IsNotEmpty()
   content: string;
+  @IsEmail()
   authorEmail: number;
 }
