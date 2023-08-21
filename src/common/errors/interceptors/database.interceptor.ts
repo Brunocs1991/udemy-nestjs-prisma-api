86ac8e8582b0c939c2436f49ec33c7a7934dbcbd
Bruno <brunocosta.1991@hotmail.com>
2023-08-21 14:03:53 -0300

    Prisma Client error

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -5,6 +5,7 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
+import { isPrismaError } from '../utils/is-prisma-error.util';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
@@ -14,6 +15,8 @@ export class DatabaseInterceptor implements NestInterceptor {
   ): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
+        if (isPrismaError(error)) {
+        }
         throw error;
       }),
     );
