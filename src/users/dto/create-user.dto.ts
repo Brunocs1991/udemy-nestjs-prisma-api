dfdb2c3ea9c79dbe4d2ccffdef1db770d32b4590
Bruno <brunocosta.1991@hotmail.com>
2023-08-21 18:27:51 -0300

    decorator @ApiProperty

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Email do usuario' })
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Nome completo do usuario' })
   name: string;
 
   @IsBoolean()
+  @ApiProperty({
+    description: 'Define se usuario é administrador',
+    default: false,
+  })
   admin: boolean;
 }
